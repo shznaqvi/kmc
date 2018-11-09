7b4c5a359aff3f03216016d28b7517d69a21f2df
ramsha.ahmed <ramshasaeed2016@gmail.com>
2018-11-09 15:44:39 +0500

    head of household added question as per hassan bhai direction

--- app/src/main/java/edu/aku/hassannaqvi/kmc/ui/SectionA1Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/kmc/ui/SectionA1Activity.java
@@ -463,6 +463,10 @@ public class SectionA1Activity extends Activity {
         }*/
 
 
+        if (!validatorClass.EmptyTextBox(this, bi.kaa11, getString(R.string.kaa11))) {
+            return false;
+        }
+
         if (!validatorClass.EmptyTextBox(this, bi.kaa05, getString(R.string.kaa05))) {
             return false;
         }
@@ -593,6 +597,7 @@ public class SectionA1Activity extends Activity {
         sInfo.put("cra07a", bi.cra07a.isChecked() ? "1"
                 : bi.cra07b.isChecked() ? "2"
                 : "0");
+        sInfo.put("kaa11", bi.kaa11.getText().toString());
         sInfo.put("kaa05", bi.kaa05.getText().toString());
         MainApp.resName = bi.kaa05.getText().toString();
         sInfo.put("kaa06", bi.kaa06.getText().toString());
