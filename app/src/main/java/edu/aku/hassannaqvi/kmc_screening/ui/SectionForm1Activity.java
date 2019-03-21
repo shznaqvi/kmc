49bfee59f56b89170116b24cc7a6e032d3f5769f
ali.azaz <ali.azaz.alam@hotmail.com>
2019-03-21 12:29:24 +0500

    Screening Forms branch created and setup its functionality

---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_screening/ui/SectionForm1Activity.java
@@ -0,0 +1,80 @@
+package edu.aku.hassannaqvi.kmc_screening.ui;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.aku.hassannaqvi.kmc_screening.JSON.GeneratorClass;
+import edu.aku.hassannaqvi.kmc_screening.R;
+import edu.aku.hassannaqvi.kmc_screening.core.DatabaseHelper;
+import edu.aku.hassannaqvi.kmc_screening.databinding.ActivitySectionForm1Binding;
+import edu.aku.hassannaqvi.kmc_screening.validation.ValidatorClass;
+
+public class SectionForm1Activity extends AppCompatActivity {
+
+    ActivitySectionForm1Binding bi;
+    String deviceID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_form1);
+        bi.setCallback(this);
+
+        setContentUI();
+    }
+
+    private void setContentUI() {
+        this.setTitle(R.string.f1_secb);
+        deviceID = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+    }
+
+    public void BtnContinue() {
+        if (!formValidation())
+            return;
+
+        try {
+            SaveDraft();
+            if (UpdateDB()) {
+
+
+            } else {
+                Toast.makeText(this, "Error in updating db!!", Toast.LENGTH_SHORT).show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean UpdateDB() {
+
+        DatabaseHelper db = new DatabaseHelper(this);
+        long count = db.updateSA1();
+        if (count != -1) return true;
+
+        Toast.makeText(this, "Error in updating DB", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    private void SaveDraft() {
+
+        JSONObject sa1 = GeneratorClass.getContainerJSON(bi.fldGrpSecB01, true);
+        SectionInfoKmcActivity.fc.setsA1(String.valueOf(sa1));
+    }
+
+    private boolean formValidation() {
+        return ValidatorClass.EmptyCheckingContainer(this, bi.fldGrpSecB01);
+    }
+
+    public void BtnEnd() {
+        startActivity(new Intent(this, EndingActivity.class));
+    }
+
+}
