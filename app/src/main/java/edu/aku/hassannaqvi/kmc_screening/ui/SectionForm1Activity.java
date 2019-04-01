18bc9b3c24eec9d525917badcb04998b525c32c0
syed.irfanullah <irfansyed.pg@gmail.com>
2019-04-01 11:05:55 +0500

    push  s

--- app/src/main/java/edu/aku/hassannaqvi/kmc_screening/ui/SectionForm1Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_screening/ui/SectionForm1Activity.java
@@ -5,9 +5,6 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -84,107 +81,7 @@ public class SectionForm1Activity extends AppCompatActivity  {
     }
 
 
-
-
-    private void setListeners() {
-
-
-        bi.kf1ab04b06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i != bi.kf1ab04b06a.getId()) {
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b07, false);
-
-                } else {
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b07, true);
-
-
-                }
-            }
-        });
-
-
-        bi.kf1ab04b07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i != bi.kf1b04b07a.getId()) {
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b08, false);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, false);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, false);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, false);
-
-                } else {
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b08,  true);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, true);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, true);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, true);
-
-
-                }
-            }
-        });
-        bi.kf1ab04b08.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i != bi.kf1b04b08a.getId()) {
-
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, false);
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, true);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, true);
-
-
-
-                } else {
-
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, true);
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, false);
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, false);
-
-
-
-                }
-            }
-        });
-
-
-
-        bi.kf1b010.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if (i != bi.kf1b10a.getId()) {
-
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, false);
-
-
-
-
-
-
-                } else {
-
-
-
-
-                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, true);
-
-                }
-            }
-        });
-
-
-    }
-
-
-           bi.kf1b010.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b10a.getId()) {
@@ -206,12 +103,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    }
 
 
+           bi.kf1b010.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-
-        bi.kf1b04a01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04a01a.getId()) {
@@ -225,11 +122,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1b04a01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-             bi.kf1b04a02.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04a02a.getId()) {
@@ -243,10 +141,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                      bi.kf1b04a03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04a02.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04a03a.getId()) {
@@ -260,10 +160,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                               bi.kf1b04a04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04a03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04a04a.getId()) {
@@ -277,11 +179,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1b04a04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                        bi.kf1b04a05.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04a05a.getId()) {
@@ -295,11 +198,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1b04a05.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                                                              bi.kf1b04b01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b01a.getId()) {
@@ -313,9 +217,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
-                              bi.kf1b04b01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            bi.kf1b04b01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b01a.getId()) {
@@ -329,10 +236,11 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
+            bi.kf1b04b01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                                       bi.kf1b04b02.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b02a.getId()) {
@@ -346,10 +254,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                                                                                                          bi.kf1b04b03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04b02.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b03a.getId()) {
@@ -363,10 +273,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                                                                                                                                                             bi.kf1b04b04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04b03.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b04a.getId()) {
@@ -380,11 +292,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1b04b04.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                                                                                                                                                                                                bi.kf1b04b05.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b05a.getId()) {
@@ -398,10 +311,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                                                                                                                                                                                                                                                                   bi.kf1b04b06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04b05.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b06a.getId()) {
@@ -415,10 +330,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                                                                                                                                                                                                                                                                                                                      bi.kf1b04b07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04b06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b07a.getId()) {
@@ -432,10 +349,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
-                                                                                                                                                                                                                                                                                                                                                                         bi.kf1ab04b06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            bi.kf1b04b07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b06a.getId()) {
@@ -449,11 +368,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1ab04b06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                                                                                                                                                                                                                                                                                                                                                         bi.kf1b04b07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b07a.getId()) {
@@ -467,11 +387,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1b04b07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                                                                                                                                                                                                                                                                                                                                                         bi.kf1b04b09.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04b09a.getId()) {
@@ -485,11 +406,12 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
 
 
+            bi.kf1b04b09.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
 
-                                                                                                                                                                                                                                                                                                                                                                         bi.kf1b04c01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
             if (i != bi.kf1b04c01a.getId()) {
@@ -503,7 +425,98 @@ public class SectionForm1Activity extends AppCompatActivity  {
 
             }
         }
-    });
+    })
+
+
+            bi.kf1b04c01.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+
+    private void setListeners() {
+
+
+        bi.kf1ab04b06.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i != bi.kf1ab04b06a.getId()) {
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b07, false);
+
+                } else {
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b07, true);
+
+
+                }
+            }
+        });
+
+
+        bi.kf1b04b07.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i != bi.kf1b04b07a.getId()) {
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b08, false);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, false);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, false);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, false);
+
+                } else {
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b08, true);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, true);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, true);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, true);
+
+
+                }
+            }
+        });
+        bi.kf1b04b08.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i != bi.kf1b04b08a.getId()) {
+
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, false);
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, true);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, true);
+
+
+                } else {
+
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b04b09, true);
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b010, false);
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, false);
+
+
+                }
+            }
+        });
+
+
+        bi.kf1b010.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if (i != bi.kf1b10a.getId()) {
+
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, false);
+
+
+                } else {
+
+
+                    ClearClass.ClearAllFields(bi.fldGrpCVkf1b11, true);
+
+                }
+            }
+        });
+
+
+    })
 
 }
 
