1866baa0124f2436f30ec104b429374b018ae39a
muhammad.fazal <fazal.abbas4@gmail.com>
2020-08-21 14:34:58 +0500

    Set mortality layout section.

---
+++ app/src/main/java/edu/aku/hassannaqvi/kmc_screening/ui/mortality/Section1Activity.java
@@ -0,0 +1,14 @@
+package edu.aku.hassannaqvi.kmc_screening.ui.mortality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Section1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section1);
+    }
+}
